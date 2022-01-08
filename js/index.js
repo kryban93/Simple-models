d5bacaf9a62ffc85b75882ab6644f2d2103d2729
kryban93 <kryban1993@gmail.com>
2022-01-08 21:30:56 +0100

    Created simple car model

--- js/index.js
+++ js/index.js
@@ -30,6 +30,51 @@ function init() {
 	renderer.render(scene, camera);
 
 	document.body.appendChild(renderer.domElement);
+
+	function createWheels() {
+		const geometry = new THREE.CylinderGeometry(7, 7, 35, 32);
+		const material = new THREE.MeshStandardMaterial({ color: '#cfcfcf' });
+		const cylinder = new THREE.Mesh(geometry, material);
+		return cylinder;
+	}
+
+	function createCar() {
+		const car = new THREE.Group();
+
+		const backWheel = createWheels();
+		backWheel.position.y = 6;
+		backWheel.position.x = -18;
+		backWheel.rotation.x = 1.57;
+		car.add(backWheel);
+
+		const frontWheel = createWheels();
+		frontWheel.position.y = 6;
+		frontWheel.position.x = 18;
+		frontWheel.rotation.x = 1.57;
+		car.add(frontWheel);
+
+		const main = new THREE.Mesh(
+			new THREE.BoxBufferGeometry(60, 15, 30),
+			new THREE.MeshLambertMaterial({ color: 0x78b14b })
+		);
+		main.position.y = 12;
+		car.add(main);
+
+		const cabin = new THREE.Mesh(
+			new THREE.BoxBufferGeometry(33, 12, 24),
+			new THREE.MeshLambertMaterial({ color: 0xffffff })
+		);
+		cabin.position.x = -6;
+		cabin.position.y = 25.5;
+		car.add(cabin);
+
+		return car;
+	}
+
+	const car = createCar();
+	scene.add(car);
+
+	renderer.render(scene, camera);
 }
 
 window.addEventListener('load', init, false);
